464485cda3e02add1f21cd1a23532a9086330c53
hmworsham <hmworsham@gmail.com>
2022-01-25 11:04:12 -0800

    removed chunkwf.pyout and updated indir for rclone_fullwf2drive.sh

--- savio_scripts/rclone_fullwf2drive.sh
+++ savio_scripts/rclone_fullwf2drive.sh
@@ -2,10 +2,10 @@
 
 # Rclone data from Savio Scratch to watched Drive directory
 
-scratch=/global/scratch/users/worsham/waveformbinarychunks/
+scratch=/global/scratch/users/worsham/waveform_binary_chunks/
 
 dest=er-drive:/EastRiver/waveformlidarchunks/
 
-priority=16
+priority=19
 
 nohup nice -n $priority rclone copy --bwlimit 8.2M $scratch $dest &
