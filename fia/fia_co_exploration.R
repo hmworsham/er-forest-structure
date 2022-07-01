8394ece367daef41c2dbbbe88ac2b1b4b01e6f3c
hmworsham <hmworsham@gmail.com>
2022-06-30 19:38:17 -0600

    feat: updated to geophys gradient scripts to include most recent plots

---
+++ fia/fia_co_exploration.R
@@ -0,0 +1,13 @@
+library(RSQLite)
+
+con <- dbConnect(RSQLite::SQLite(), '~/Downloads/FIADB_CO.db')
+
+dbListFields(con, 'COND')
+
+counties <- dbReadTable(con, 'COUNTY')
+cond <- dbReadTable(con, 'COND')
+
+gunnison <- cond[(cond$STATECD=8) & (cond$COUNTYCD==51),]
+View(gunnison)
+length(unique(gunnison$PLOT))
+       
