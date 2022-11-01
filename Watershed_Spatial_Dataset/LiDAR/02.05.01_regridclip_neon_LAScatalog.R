7fc385b96dc7e9fbdf28a6741373dc9294b4d976
hmworsham <hmworsham@gmail.com>
2022-11-01 16:36:55 -0600

    removed regrid and clip function for neon point cloud processing and added to a new script

--- Watershed_Spatial_Dataset/LiDAR/02.05.01_regridclip_neon_LAScatalog.R
+++ Watershed_Spatial_Dataset/LiDAR/02.05.01_regridclip_neon_LAScatalog.R
@@ -30,24 +30,27 @@ neondir <- '/global/scratch/users/worsham/neon_las'
 ngdir <- '/global/scratch/users/worsham/neon_las_gaps'
 dir.create(outdir)
 
-######################
-# Regrid las catalog
-######################
-lascat <- readLAScatalog(datadir)
-opt_chunk_buffer(lascat) <- 0
-opt_chunk_size(lascat) <- 500
-opt_laz_compression(lascat) <- T
-opt_output_files(lascat) <- file.path(outdir, "las_regridded_{XLEFT}_{YBOTTOM}")
+#############################################
+# Pull in neon points to fill gaps and regrid
+#############################################
 
-# Preview the chunk pattern
-lascat <- lascat[lascat['Number.of.point.records']$Number.of.point.records>0,]
-plot(lascat['Number.of.point.records'])
+# Ingest gap shapefile
+fp.gap <- vect('/global/scratch/users/worsham/EastRiver/missing_flightpath.shp')
+fp.gap <- st_as_sf(fp.gap)
 
-# Retile with multiple cores
-plan(multisession, workers = 30L)
-set_lidr_threads(30L)
-newlascat <- catalog_retile(lascat)
+# Ingest neon point cloud las catalog
+neoncat <- readLAScatalog(list.files(neondir, full.names=T, pattern='*2018062013*')[c(1:5, 13)])
 
-# Check regridded las catalog
-lascatrg <- readLAScatalog(list.files(outdir, full.names=T, pattern='.laz'))
-plot(lascatrg['Number.of.point.records'])
+# Grid the gap polygon for faster processing
+gridz <- st_make_grid(fp.gap, cellsize=500)
+gridz <- st_intersection(fp.gap, gridz)
+gridz <- st_as_sf(vect(gridz))
+
+# Set up parallel and output parameters
+plan(multisession, workers = 24L)
+set_lidr_threads(24L)
+opt_laz_compression(neoncat) <- T
+opt_output_files(neoncat) <- file.path(outdir, 'neongaps', 'neongaps_las_regridded_{XLEFT}_{YBOTTOM}')
+
+# Clip neon points to gap polygon and regrid, writing to file
+gaps <- clip_roi(neoncat, gridz)
