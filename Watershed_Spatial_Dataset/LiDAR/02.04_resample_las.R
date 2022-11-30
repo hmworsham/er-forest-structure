a7a2190ec40dfd6eb164fd7960e01134fb12aceb
hmworsham <hmworsham@gmail.com>
2022-11-30 15:02:07 -0800

    refactored resample_las to .25 downsampling and completeness checking

--- Watershed_Spatial_Dataset/LiDAR/02.04_resample_las.R
+++ Watershed_Spatial_Dataset/LiDAR/02.04_resample_las.R
@@ -9,7 +9,7 @@ library(rlas)
 
 shapedir <- '/global/scratch/users/worsham/EastRiver/RMBL_2020_EastRiver_SDP_Boundary'
 datadir <- '/global/scratch/users/worsham/las_regridded'
-outdir <- '/global/scratch/users/worsham/las_resampled2'
+outdir <- '/global/scratch/users/worsham/las_downsampled'
 dir.create(outdir)
 
 ########################
@@ -24,9 +24,23 @@ resample_las <- function(las, outdir){
   xx@data = rbind(pk, npk)
   xx@header = header_update(xx@header, xx@data)
 
-  newlas = basename(las)
+  newlas = str_replace(basename(las), 'regridded', 'downsampled')
   newlas = file.path(outdir, newlas)
   write.las(newlas, xx@header, xx@data)
 }
 
-mclapply(list.files(datadir, full.names=T), resample_las, outdir, mc.cores=getOption('mc.cores', 24L))
+did <- list.files(outdir)
+inputs <- list.files(datadir)
+notdid <- file.path(datadir, inputs[!inputs %in% did])
+
+mclapply(notdid, resample_las, outdir, mc.cores=getOption('mc.cores', 24L))
+
+lascat <- readLAScatalog(file.path(outdir, did))
+plot(lascat['Number.of.point.records'])
+plot(lascat[lascat['Number.of.point.records']$Number.of.point.records>0])
+
+errs <- which(lascat['Number.of.point.records']$Number.of.point.records==0)
+errs <- c(99,192,193,194,804,1063,1064,1065)
+errs <- file.path(datadir, inputs[errs])
+errs
+mclapply(errs, resample_las, outdir, mc.cores=getOption('mc.cores', 24L))
