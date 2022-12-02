ae9082bad627985851f07dae5b76332a78405c61
hmworsham <hmworsham@gmail.com>
2022-12-02 15:53:30 -0800

    added handling for field data, splitting plots into quads, and its algorithm structure

--- Watershed_Spatial_Dataset/LiDAR/05.00_itc_traintest.R
+++ Watershed_Spatial_Dataset/LiDAR/05.00_itc_traintest.R
@@ -14,17 +14,13 @@ pkgs <- c('dplyr',
           'raster',
           'devtools',
           'plotly',
-          'rPeaks',
-          'rgdal',
-          'caTools',
           'sf', 
           'terra',
           'parallel',
-          'itcSegment',
           'lidR',
+          'lidRplugins',
           'rlas',
           'rgl',
-          'nngeo',
           'broom',
           'plot3D',
           'readxl') # Name the packages you want to use here
@@ -42,9 +38,8 @@ load.pkgs(pkgs)
 load_all('~/Repos/rwaveform')
 
 # Name directories
-#datadir <- '/global/scratch/users/worsham/geolocated_returns_plots'
 scrdir <- file.path('/global', 'scratch', 'users', 'worsham')
-datadir <- file.path(scrdir, 'las_decimated2')
+datadir <- file.path(scrdir, 'las_decimated')
 regriddir <- file.path(scrdir, 'las_regridded')
 shapedir <- file.path(scrdir, 'EastRiverInputs', 'Plot_Shapefiles', 'AllPlots')
 fidir <- file.path(scrdir, 'EastRiverInputs', 'Inventory_Plots')
@@ -52,86 +47,208 @@ gpsdir <- file.path(scrdir, 'EastRiverInputs', 'StemGeolocations')
 outdir <- file.path(scrdir, 'trees')
 dir.create(outdir)
 
-#########################################
-# Ingest points from waveform processing
-########################################
+################
+# Ingest data
+################
 
-#ptcsv <- list.files(datadir, full.names = T)
-#aois <- list.files(datadir, full.names = F)
-#aois <- unique(sapply(strsplit(aois, '_2018'), '[', 1))
-
+# Ingest full las catalog
 infiles <- list.files(datadir, full.names=T)
 lascat <- readLAScatalog(infiles)
 
+# Ingest plot boundaries
 plotsf <- st_read(list.files(shapedir, pattern='.shp', full.names=T))
 aois <- plotsf$PLOT_ID
+aois <- c('SG-SWR1', 
+          'CC-UC1', 
+          'WG-WGM1', 
+          'ER-APL1', 
+          'CC-UC2', 
+          'SG-NES2', 
+          'ER-BME2', 
+          'ER-BME1', 
+          'ER-APU1', 
+          'ER-GT1', 
+          'SG-NES3', 
+          'SR-PVG1', 
+          'SG-NES1',
+          'CC-CVN2', 
+          'CC-EMN1', 
+          'CC-CVS1', 
+          'CC-CVN2')
 
-plot(lascat)
-plot(plotsf, add=T, col='red')
+# Split plots into quadrants
+div <- 2 # Number of divisions
+ls <- list() # Empty list to store result
 
-las1 <- readLAS(infiles[1200])
-plot(extent(las1))
-plot(extent(las1)-5, col='red', add=T)
-ex <- ext(las1)-5 
-las1c <- clip_rectangle(las1, ex[1], ex[2], ex[3], ex[4])
-plot(las1, bg='white')
-rglwidget()
+for (i in 1:nrow(plotsf)){
+  x <- st_make_grid(plotsf[i,], n = div) %>% st_as_sf() # divide pol
+  xx <- st_intersection(plotsf[i,], x) # intersection
+  xx$QUADRANT <- sapply(seq(1:4), function(x) paste(plotsf[i,]$PLOT_ID, x, sep='.'))
+  ls[[i]] <- xx
+}
+plotsf <- sf::st_as_sf(data.table::rbindlist(ls)) # superfast
+aoi.quads <- paste(unlist(lapply(aois, rep, 4)), seq(1,4), sep='.')
 
-plot(extent(las1), col='red')
-plot(extent(las2), col='blue', add=T)
+
+# Ingest field data
+invfiles <- list.files(fidir, pattern='_inventory_data_20', recursive=T, full.names=T)
+invfiles
+inv = read_excel(invfiles[1], sheet='inventory_data')
+df = data.frame('Z'=as.numeric(inv$Height_Avg_M), 'X'=as.numeric(inv$Longitude), 'Y'=as.numeric(inv$Latitude))
+df = na.omit(df)
+
+#############################
+# Plotting for verification
+#############################
+
+# Plot las catalog with plot boundaries
+# plot(lascat)
+# plot(plotsf, add=T, col='red')
+# plot(plotsf[1,1])
+# extent(plotsf[1,1])
+
+# Read in one las file
+# las1 <- readLAS(infiles[960])
+
+# Crop las by 5m square [not sure why this is in here...]
+# ex <- extent(las1)-5
+# las1c <- clip_rectangle(las1, ex[1], ex[3], ex[2], ex[4])
+
+# Plot cropped las
+# plot(las1c, bg='white')
+# rglwidget()
 
 ##########################################
-# ITC segmentation
+# ITS optimization
 ##########################################
+
+# Ingest point cloud at all plots with a given buffer
+lasplots <- mclapply(aoi.quads, function(x){
+  p = plotsf[plotsf$QUADRANT==x,][1]
+  bnd = st_buffer(p$geometry, 5)
+  pc = clip_roi(lascat, bnd)
+  return(pc)
+  },
+  mc.cores = getOption("mc.cores", 64L))
+
+# Check
+assertthat::are_equal(length(lasplots), length(aoi.quads), 68)
+
+# Define vectors of parameters on which to run Li algorithm for optimization
+dt1.seq = seq(0.1, 0.5, 0.2)
+dt2.seq = seq(1, 2, 0.2)
+R.seq = seq(0, 3, 1)
+Zu.seq = seq(14, 16, 1)
+length(dt1.seq)*length(dt2.seq)*length(R.seq)*length(Zu.seq)
+
+g <- expand.grid(dt1.seq, dt2.seq, R.seq, Zu.seq)
+
+# operate on each row of grid
+# do.call(mapply, c("f", unname(as.list(g))))
+# # or
+# sapply(1:nrow(g), function(i) do.call("f", unname(g[i, ])))
+# # or
+# mapply(f, g[, 1], g[, 2])
+
+workerNodes <- c(rep('n0014.savio3',36), rep('n0243.savio3', 36))
+cl <- parallel::makeCluster(workerNodes)
+
+# Initialize Li 2012
+li2012.opt <- function(pc, dt1, dt2, R, Zu, hmin=1.3, threads=30L, workers=30L){
+  algo = li2012(dt1, dt2, R, Zu, hmin)
+  segtrees = segment_trees(pc, algo) # segment point cloud
+  return(segtrees)
+}
+
+testli <- mcmapply(li2012.opt, 
+                   g[,1], 
+                   g[,2], 
+                   g[,3], 
+                   g[,4], 
+                   MoreArgs=list(pc=lasplots[[1]], hmin=1.3),
+                   mc.cores = getOption("mc.cores", 64L))
+
+# or
+do.call(mapply, c("f", unname(as.list(g))))
+
+# testli <- mapply(li2012.opt, dt1.seq, dt2.seq, R.seq, Zu.seq, MoreArgs=list(pc=pointcloud.test[[1]], hmin=1.3))
+
+vrep <- Vectorize(rep.int)
+vrep(1:4, 4:1)
+vrep(times=1:4, x=4:1)
+
+# Initialize Dalponte 2016
+# Rasterize canopy surface from point cloud in AOI
+chm <- rasterize_canopy(subset1, 0.5, pitfree(subcircle = 0.2))
+
+
+##########################################
+# ITC segmentation on one AOI
+##########################################
+
+# Specify AOI
 aoi <-  'SG-NES2'
 
-getcrowns <- function(aoi){
-outtmp <- tempdir()
-aoipts <- write.csv(do.call('rbind', lapply(ptcsv[grep(aoi, ptcsv)], read.csv)), file.path(outtmp, 'aoi.csv'))
-pts2las(list.files(outtmp, full.names=T)[grep('aoi.csv', list.files(outtmp))], outtmp)
-testlas <- readLAS(list.files(outtmp, full.names=T)[grep('aoi.las', list.files(outtmp))])
-plot(testlas)
-rglwidget()
-
-# Pull points in plot bound
+# Ingest points at plot bound with specified buffer
 plotsf <- plotsf[plotsf$PLOT_ID==aoi,][1]
-testgeom <- st_buffer(plotsf$geometry, 100)
-subset1 <- clip_roi(lascat, plotsf)
+testgeom <- st_buffer(plotsf$geometry, 10)
+subset1 <- clip_roi(lascat, testgeom)
+
+p = plotsf[plotsf$PLOT_ID=='SG-SWR1',][1]
+bnd = st_buffer(p$geometry, 10)
+pc = clip_roi(lascat, bnd)
+
+# Plot point cloud at aoi
 plot(subset1, bg='white', size=2)
 rglwidget()
 
+# Rasterize canopy surface from point cloud in AOI
 chm <- rasterize_canopy(subset1, 0.5, pitfree(subcircle = 0.2))
-plot(chm, col = height.colors(50))
+#plot(chm, col = height.colors(50))
 
-sgtrees <- locate_trees(subset1, lmf(ws = 2.2))
-plot(sf::st_geometry(sgtrees), add = TRUE, pch = '+')
+# Find tree tops from point cloud in AOI
+sgtrees <- locate_trees(subset1, lmf(ws = 2.1))
+#plot(sf::st_geometry(sgtrees), add = TRUE, pch = '+')
 
+# Plot point cloud with identified treetops
 x <- plot(subset1, bg = "white", size = 4)
 add_treetops3d(x, sgtrees)
 rglwidget()
+
+# Segment trees using Dalponte 2016
 st_crs(sgtrees) <- 32613
-algo <- dalponte2016(chm, sgtrees, th_tree=2, th_seed=0.15)
-algo <- li2012()
+algo <- dalponte2016(chm, sgtrees, th_tree=1.3, th_seed=0.005)
+crowns <- algo()
+
+plot(crowns, col=pastel.colors(200))
 
 segtrees <- segment_trees(subset1, algo) # segment point cloud
-
+length(unique(segtrees@data$treeID))
 plot(segtrees, bg = "white", size = 4, color = "treeID") # visualize trees
 rglwidget()
 
-length(unique(segtrees@data$treeID))
 crowns <- crown_metrics(segtrees, func = .stdtreemetrics, geom = "convex")
-st_is_valid(crowns)
-crowns <- st_make_valid(crowns)
+crowns <- crowns[st_is_valid(crowns),]
+plot(crowns["convhull_area"], main = "Crown area (convex hull)", col=pastel.colors(nrow(crowns)))
 
-plot(sf::st_geometry(crowns), reset = FALSE)
-plot(chm)
-plot(crowns["convhull_area"], main = "Crown area (convex hull)", col=NA, add=T)
-plot(algo(), col = pastel.colors(460))
-crowns
+# Segment trees using Li 2012
+algo <- li2012(dt=.01, dt2=.02, R=3, hmin=1.3)
+segtrees <- segment_trees(subset1, algo) # segment point cloud
+length(unique(segtrees@data$treeID))
+
+tree112 <- filter_poi(segtrees, treeID == 17)
+plot(tree112)
+rglwidget()
+
+crowns <- crown_metrics(segtrees, func = .stdtreemetrics, geom = "convex")
+#crowns <- st_make_valid(crowns)
+crowns <- crowns[st_is_valid(crowns),]
+nrow(crowns)
+
+plot(crowns["convhull_area"], main = "Crown area (convex hull)", col=pastel.colors(nrow(crowns)))
+
 plot(plotsf, add=T)
 xx <- predictloss(fidir, shapedir, sgtrees, aoi)
-
-?crown_metrics
 
 itcfun <- function(aoi){
   olas = pts2las(datadir, aoi)
