d7be00a3ed3b0c9d86931c4450695f6e9958a33a
hmworsham <hmworsham@gmail.com>
2023-02-15 11:01:49 -0700

    add load.pkgs to helpers.R

---
+++ er-forest-inventory/R/helpers.R
@@ -0,0 +1,7 @@
+# Function to install new packages if they're not already installed
+load.pkgs <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
