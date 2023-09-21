ff01a82b2fbeeb33fc19afc13bf98b9814d28fb4
hmworsham <hmworsham@gmail.com>
2023-09-21 15:49:05 -0600

    include ba

--- inst/notebooks/regressions/01.00_stats_ingest_data.R
+++ inst/notebooks/regressions/01.00_stats_ingest_data.R
@@ -11,6 +11,8 @@ load.pkgs(config$pkgs)
 # Set up working environment
 #############################
 
+# TODO: refactor to use googledrive ingest
+
 # Define directories
 datadir <- file.path('/Volumes', 'GoogleDrive', 'My Drive', 'Research', 'RMBL', 'RMBL-East River Watershed Forest Data', 'Data')
 strdir <- file.path(datadir, 'LiDAR', 'tifs')
@@ -33,40 +35,20 @@ aop <- st_read(file.path(sfdir, 'SDP_Boundary.shp'))
 
 #### Forest structure ####
 # Ingest forest structure rasters
-dnsty <- raster(file.path(strdir, 'stand_density_100mx.tif'))
+dnsty <- raster(file.path(strdir, 'stand_density_100m_fromtrees.tif'))
 height <- raster(file.path(strdir, 'height_90pctl.tif'))
 diam <- raster(file.path(strdir, 'mean_diam_100m.tif'))
-response <- list('density'=dnsty, 'height'=height, 'diam'=diam)
-
-# PLOT RESPONSE VARIABLES!!!!!
-list.files(strdir)
-responses <- lapply(list.files(strdir, full.names=T, pattern='*.tif$')[1:9], raster)
-responses[[10]] <- (responses[[7]]/2)**2*pi
-names(responses[[10]]) <- 'basal-area'
-values(responses[[9]])[values(responses[[9]]==1)] <- NA
-par(mfrow=c(2,3),
-    mar=c(1,1,1,1)+.5)
-names <- str_replace(list.files(strdir)[1:9], '.tif', '')
-names <- str_replace(names, '_fromtrees', '')
-names <- str_replace(names, '_', ' ')
-names <- str_replace(names, '_', ' ')
-names[10] <- 'basal_area'
-
-toplot <- c(9, 10, 5,8,7,9)
-
-for(i in seq_along(toplot)) {
-  terra::plot(raster::projectRaster(responses[[toplot[i]]], crs='EPSG:4326'),
-              col=viridis(n=20, option=LETTERS[i]),
-              main=names[toplot[i]],
-              title(names[toplot[i]], line=2, cex=3),
-              cex.main=3
-              )
-}
+# ba <- raster(file.path(strdir, 'basal_area_100m.tif'))
+response <- list(
+  'density'=dnsty,
+  'height'=height,
+  'diam'=diam #,
+  # 'ba'=ba
+  )
 
 # Check forest structure rasters
-crs(dnsty)
-values(dnsty)
-plot(dnsty, col=viridis(9))
+lapply(response, crs)
+lapply(response, plot, col=viridis(9))
 
 #### Topographic variables ####
 # Define topographic factors to ingest
