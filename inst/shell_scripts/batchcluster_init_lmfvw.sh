b26cab924e2081989ec7a091bf13ed988388188e
hmworsham <hmworsham@gmail.com>
2023-10-12 20:51:20 -0700

    create slurm script for new itc algorithms

---
+++ inst/shell_scripts/batchcluster_init_lmfvw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=itc_traintest
+#SBATCH --account=fc_lmklab
+#SBATCH --partition=savio3
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=08:00:00
+
+## Command(s) to run
+module load r r-packages r-spatial
+while true
+do
+R CMD BATCH --no-save \
+../notebooks/LiDAR/05.06_itc_lmf_vw.R \
+/global/scratch/users/worsham/logs/Rout_itc_lmfvw.Rout
+done
