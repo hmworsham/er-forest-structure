11129aa1819e0d0f56c1e20cc5867d6fb7b5ae9c
hmworsham <hmworsham@gmail.com>
2023-10-12 18:38:20 -0700

    refactor to run parameterless algorithm

--- R/lmfauto_funs.R
+++ R/lmfauto_funs.R
@@ -2,33 +2,30 @@
 
 #' LMF fixed window initialization
 #' @description Initialize LMF fixed window for optimization
-#' @export lmf.fw.init
+#' @export lmf.auto.init
 #'
-lmf.fw.init <- function(pc, ws, shape, hmin=1.3){
-  algo = lmf(ws=ws, shape=shape, hmin=hmin)
-  lmf.fw.trees <- find_trees(pc, algo)
-  return(lmf.fw.trees)
+lmf.auto.init <- function(pc, hmin=1.3){
+  algo = lmfauto(hmin=hmin)
+  lmf.auto.trees <- find_trees(pc, algo)
+  return(lmf.auto.trees)
 }
 
 #' LMF fixed window optimization
 #' @description Run LMF fixed window optimization algorithm across sample areas
-#' @export lmf.fw.opt
+#' @export lmf.auto.opt
 #'
-lmf.fw.opt <- function(x, params) {
 
-  modtrees <- mapply(lmf.fw.init,
-                       ws=params[,1][[1]],
-                       shape=params[,2][[1]],
-                       MoreArgs=list(pc=x, hmin=1.3) #,
-                       # mc.cores = getOption("mc.cores", length(workerNodes)-2)
-  )
+lmf.auto.opt <- function(x, hmin) {
+
+  modtrees <- lmf.auto.init(x, hmin=hmin)
+  modtrees <- st_as_sf(modtrees)
 
   # Clean up results
-  modtrees <- lapply(modtrees, st_as_sf)
-  modtrees <- lapply(modtrees, function(x) {
-    tl <- x[!st_is_empty(x),]
-    tl
-  })
+  # modtrees <- lapply(modtrees, st_as_sf)
+  # modtrees <- lapply(modtrees, function(x) {
+  #   tl <- x[!st_is_empty(x),]
+  #   tl
+  # })
 
   return(modtrees)
 }
