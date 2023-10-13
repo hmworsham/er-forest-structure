6499e0f8407c08047579c889645a4775427feed3
hmworsham <hmworsham@gmail.com>
2023-10-12 18:40:33 -0700

    refactor to use multiple parameters in vws function

--- R/lmfvw_funs.R
+++ R/lmfvw_funs.R
@@ -4,20 +4,21 @@
 #' LMF variable window function
 #' @description Define variable window as thresholded exponential f(height)
 #' @export vws
-vws = function(x, p1, p2, p3) {
-  y <- p1 * (-(exp(-p2*(x-2)) - 1)) + p3
-  y[x < 2] <- 3
-  y[x > 20] <- 5
-  return(y)
-}
 
 #' LMF variable window initialization
 #' @description Initialize LMF fixed window for optimization
-#' @export ls.init
+#' @export lmf.vw.init
 #'
 # Define variable window size function
-lmf.vw.init <- function(pc, ws, p1, p2, p3, shape, hmin=1.3) {
-  algo = lmf(ws=vws(p1,p2,p3), shape=shape, hmin=hmin)
+lmf.vw.init <- function(pc, b0, b1, c0, shape, hmin=1.3) {
+  vws = function(z, p1, p2, p3) {
+    y <- p1 * (-(exp(-p2*(z-2)) - 1)) + p3
+    y[z < 2] <- 3
+    y[z > 20] <- 5
+    return(y)
+  }
+  ws_args = list(z='Z', p1=b0, p2=b1, p3=c0)
+  algo = lmf(ws=vws, shape=shape, hmin=hmin, ws_args=ws_args)
   lmf.vw.trees <- find_trees(pc, algo)
   return(lmf.vw.trees)
 }
@@ -25,15 +26,18 @@ lmf.vw.init <- function(pc, ws, p1, p2, p3, shape, hmin=1.3) {
 # TODO: WRITE THIS UP
 #' LMF fixed window optimization
 #' @description Run LMF fixed window optimization algorithm across sample areas
-#' @export ls.opt
+#' @export lmf.vw.opt
 #'
 lmf.vw.opt <- function(x, params) {
   modtrees <- mapply(lmf.vw.init,
-                     ws=params[,1][[1]],
-                     shape=params[,2][[1]],
+                     b0=params[,1][[1]],
+                     b1=params[,2][[1]],
+                     c0=params[,3][[1]],
+                     shape=params[,4][[1]],
                      MoreArgs=list(pc=x, hmin=1.3)#,
-                     # mc.cores=getOption('mc.cores', 30)
-  )
+                     # mc.cores = getOption("mc.cores", length(workerNodes)-2)
+                     )
+
 
   # Clean up results
   modtrees <- lapply(modtrees, st_as_sf)
