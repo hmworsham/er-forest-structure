dcf1de67206e0adc43057601777aa206ba99f9e9
hmworsham <hmworsham@gmail.com>
2023-10-12 20:52:00 -0700

    time 08:00:00

--- inst/shell_scripts/multibatch_init.sh
+++ inst/shell_scripts/multibatch_init.sh
@@ -3,7 +3,11 @@ sbatch ./batchcluster_init_li.sh
 sbatch ./batchcluster_init_pt.sh
 sbatch ./batchcluster_init_ls.sh
 sbatch ./batchcluster_init_mc.sh
-sbatch ./batchcluster_init_lmf_fw.sh
+sbatch ./batchcluster_init_lmffw.sh
+sbatch ./batchcluster_init_lmfvw.sh
+sbatch ./batchcluster_init_lmfauto.sh
+sbatch ./batchcluster_init_ws.sh
+
 #sbatch ./batchcluster_init_06.sh
 #sbatch ./batchcluster_init_07.sh
 #sbatch ./batchcluster_init_08.sh
