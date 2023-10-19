98bb02d6efa513b29c05ca415481344aa70c18b9
hmworsham <hmworsham@gmail.com>
2023-10-19 10:44:48 -0700

    run with lmffw

--- inst/notebooks/LiDAR/05.11_detect_trees_full_watershed.R
+++ inst/notebooks/LiDAR/05.11_detect_trees_full_watershed.R
@@ -18,8 +18,7 @@ drive_auth(path=config$drivesa)
 ## ---------------------------------------------------------------------------------------------------
 workerNodes <- str_split(system('squeue -u $USER -o "%N"', intern=T)[[2]], ',', simplify=T)
 workerNodes <- rep(workerNodes, 32)
-cl <- parallel::makeCluster(workerNodes)
-#set_lidr_threads(length(workerNodes)-2)
+set_lidr_threads(length(workerNodes)-2)
 
 ## ---------------------------------------------------------------------------------------------------
 # Specify directories
@@ -40,17 +39,22 @@ res = 0.5
 ws1 = 2
 ws2 = 2
 buf = 0.2
+hmin = 1.3
+
+# Specify optimal lmf-fw parameters
+# ws=2.4
+# shape='circular'
 
 ## ---------------------------------------------------------------------------------------------------
 # Specify output location
 opt_output_files(lascat) <- file.path(outdir, 'trees_{XLEFT}_{YBOTTOM}')
-opt_chunk_size(lascat) <- 250
-opt_chunk_buffer(lascat) <- 10
+opt_chunk_size(lascat) <- 500
+opt_chunk_buffer(lascat) <- 25
 
 # Detect trees using optimal algorithm and parameters
-plan(multisession, workers=20L)
-ls.init(lascat[501,], start=0.5, res=0.5, ws1=2, ws2=2, buf_size=0.2, hmin=1.3, hardwood=F)
-algo <- LayerStacking(start=0.5, res=0.5, ws1=2, ws2=2, buf_size=0.2, hmin=1.3, hardwood=F)
-ls.trees <- locate_trees(lascat[501:505,], algo, uniqueness='bitmerge')
+plan(multisession, workers=24L)
 
-plot(ttops["Z"], cex = 0.001, pch = 19, pal = height.colors, nbreaks = 30)
+#ls.init(lascat[60,], start=0.5, res=0.5, ws1=2, ws2=2, buf_size=0.2, hmin=1.3, hardwood=F)
+algo <- LayerStacking(start=0.5, res=0.5, ws1=2, ws2=2, buf_size=0.2, hardwood=F, hmin=1.3)
+#algo2 <- lmf(ws=ws, shape=shape, hmin=1.3)
+ls.trees <- find_trees(lascat[1:110,], algo, uniqueness='incremental')
