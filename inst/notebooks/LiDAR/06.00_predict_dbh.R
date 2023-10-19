a735e9d4d5b700e6034ce87157f63ea369650832
hmworsham <hmworsham@gmail.com>
2023-10-19 10:45:23 -0700

    use site allometries to predict DBH, BA, and their uncertainties

--- inst/notebooks/LiDAR/06.00_predict_dbh.R
+++ inst/notebooks/LiDAR/06.00_predict_dbh.R
@@ -1,51 +1,106 @@
-library(terra)
+# Predict DBH on modeled trees
 
-datadir <- '/global/scratch/users/worsham/'
-plotsdir <- '/global/scratch/users/worsham/geolocated_returns_plots'
-shapedir <- '/global/scratch/users/worsham/EastRiver/Plot_Shapefiles/Polygons'
-fidir <- '/global/scratch/users/worsham/EastRiver/Inventory_Plots'
+## ---------------------------------------------------------------------------------------------------
+# Load config
+config <- config::get(file=file.path('~',
+                                     'Repos',
+                                     'er-forest-structure',
+                                     'config',
+                                     'config.yml'))
 
-sgtrees <- read.csv
-list.files(datadir)
+# Load local helper functions and packages
+devtools::load_all()
+load.pkgs(config$pkgs)
 
-aois <- list.files(plotsdir, full.names = F)
-aois <- unique(sapply(strsplit(aois, '_2018'), '[', 1))
+## ---------------------------------------------------------------------------------------------------
+drive_auth(path=config$drivesa)
 
-predictdbh <- function(aoi, inventorydir, shapedir) {
-  
-  # Get plot boundary for aoi
-  plotpath = list.files(shapedir, 
-                        pattern = glob2rx(paste0(aoi,"*shp")),
-                        full.names = T)
-  print(plotpath)
-  plotsf = vect(plotpath)
-  geoextent = as.list(ext(plotsf))
-  
-  invfiles = list.files(fidir, pattern = paste0(aoi,'_inventory_data_20'), recursive=T, full.names = T)
-  inv = read_excel(invfiles[1], sheet='inventory_data')
-  df = data.frame('Z'=as.numeric(inv$Height_Avg_M), 'X'=as.numeric(inv$Longitude), 'Y'=as.numeric(inv$Latitude), 'D'=as.numeric(inv$DBH_Avg_CM))
-  df = na.omit(df)
-  return(df) 
+# Ingest trees
+#treefiles <- list.files(config$extdata$trees)
+treefiles <- list.files('/global/scratch/users/worsham/trees_lmffw', pattern='.shp', full.names=T)
+treefiles.mfp <- list.files('/global/scratch/users/worsham/trees_lmffw_missing_fp', pattern='.shp', full.names=T) # tree files from original NEON LAS for missing flightpath
+treefiles <- c(treefiles, treefiles.mfp)
+
+# Ingest field data
+tmpfile <- drive_download(
+  as_id(config$extdata$invid),
+  type='csv',
+  path=file.path(tempdir(), config$extdata$invid),
+  overwrite=T)$local_path
+
+inv <- read.csv(tmpfile)
+
+# Select the variables of interest
+allom <- inv %>%
+  filter(!Site_Name %in% c('XX-CAR1', 'XX-CAR2', 'XX-CAR3',
+                           'XX-PLN1', 'XX-PLN2', 'SG-NWS1',
+                           'XX-FAR1', 'ER-BME3')) %>%
+  dplyr::select(Site_Name,
+         Height_Avg_M,
+         DBH_Avg_CM) %>%
+  na.omit()
+
+# Scatterplot with a smooth fit, grouped by Site_Name
+ggplot(allom, aes(x=DBH_Avg_CM, y=Height_Avg_M)) +
+  geom_point(shape=21) +
+  geom_smooth(se=F) +
+  labs(x='DBH (cm)', y='Height (m)')
+
+# Fit exponential model to field height and diameter
+nthroot = function(x,n) {
+  (abs(x)^(1/n))*sign(x)
 }
 
-xx <- lapply(aois, predictdbh, fidir, shapedir)
-plotdata <- rbindlist(xx)
-coef <- lm(D ~ log(Z), data=plotdata)
-resid <- resid(coef)
-summary(coef)
-plot(coef)
+nlmod <- nls(DBH_Avg_CM~nthroot(Height_Avg_M/a, b),
+             start=list(a=0.1, b=0.01),
+             data=allom,
+             na.action=na.exclude,
+             control=nls.control(maxiter=1000))
 
-plot(plotdata$Z, plotdata$D)
-plot(plotdata$Z, resid)
-abline(0,0)
-plot(1:50, -8.1946+12.2768*log(1:50), type='l')
-points(plotdata$Z, plotdata$D)
+# nlmod <- nls(Height_Avg_M~a*DBH_Avg_CM^b,
+#              start=list(a=0.1, b=0.01),
+#              data=allom,
+#              na.action=na.exclude,
+#              control=nls.control(maxiter=1000))
 
-# On one plot
-xx <- predictdbh(fidir, shapedir, sgtrees, aoi)
-coef <- lm(D ~ log(Z), data=xx)
-resid <- resid(coef)
-summary(coef)
-plot(xx$Z, xx$D)
-plot(xx$Z, resid)
-abline(0,0)
+# Pull coefficients
+coef <- summary(nlmod)$coefficients
+a <- coef[1,'Estimate']
+b <- coef[2, 'Estimate']
+a.se <- coef[1, 'Std. Error']
+b.se <- coef[2, 'Std. Error']
+
+# Predict diameter from height in field data
+#yhat <- predict(nlmod, allom$DBH_Height_M)
+yhat <- nthroot(allom$Height_Avg_M/a, b)
+allom$yhat <- yhat
+resid <- allom$yhat-allom$DBH_Avg_CM
+
+# What is RMSE of DBH estimate?
+dbh.est.rmse <- sqrt(mean(resid^2))
+dbh.est.rmse
+
+# Plot predictions
+ggplot(allom, aes(x=Height_Avg_M)) +
+  geom_point(aes(y=DBH_Avg_CM), shape=21, color='blue') +
+  geom_point(aes(y=yhat), shape=21, color='red') +
+  geom_line(aes(y=yhat))
+
+# Predict diameter from height in model data and write to csv
+#trees <- bind_rows(map(treefiles, st_read))
+
+mclapply(treefiles, \(x) {
+  tf <- st_read(x)
+  tf$DBH_est <- nthroot(tf$Z/a, b)
+  tf$DBH_lb <- nthroot(tf$Z/(a-a.se), b-b.se)
+  tf$DBH_ub <- nthroot(tf$Z/(a+a.se), b+b.se)
+  tf$BA_est <- pi*(tf$DBH_est/2)**2
+  tf$BA_est_lb <- pi*(tf$DBH_lb/2)**2
+  tf$BA_est_ub <- pi*(tf$DBH_ub/2)**2
+  outname <- str_split(basename(x), '\\.', simplify=T)[1]
+  st_write(data.frame(tf), file.path('/global/scratch/users/worsham/trees_lmffw_csv', paste0(outname, '.csv')), layer_options = "GEOMETRY=AS_XY")
+},
+mc.cores = getOption("mc.cores", 30)
+)
+
+
