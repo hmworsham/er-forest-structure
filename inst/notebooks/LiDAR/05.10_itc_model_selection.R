1abe23facc7e1b22650364ea9cade99d7eb31fb5
hmworsham <hmworsham@gmail.com>
2023-11-01 10:08:35 -0700

    updates to report performance in new tables

--- inst/notebooks/LiDAR/05.10_itc_model_selection.R
+++ inst/notebooks/LiDAR/05.10_itc_model_selection.R
@@ -51,7 +51,6 @@ optmod <- itc.means[which.max(itc.means$f),]
 optmod
 #optmod <- itc.means[which.min(itc.means$loss),]
 
-
 ggplot(itc.res, aes(x=model, y=f)) +
   geom_boxplot() +
   facet_wrap(~quad)
@@ -63,16 +62,63 @@ ggplot(chkmod, aes(x=quad, y=f)) +
 
 
 # Generate summary stats for optimal model
-opt.res <- itc.res[itc.res$model=='ls' & itc.res$paramset==11,]
+opt.res <- itc.res[itc.res$model=='ls' & itc.res$paramset==7,]
 
-opt.sum <- summary(opt.res)
-opt.sum
 opt.mean <- opt.res %>%
-  summarise(across(nobstrees:loss, \(x) mean(x, na.rm=T)))
+  summarise(across(nobstrees:npredtrees, \(x) sum(x, na.rm=T)),
+            across(c(ext.rt, match.rt, accuracy:f), \(x) round(sqrt(mean(x^2, na.rm=T)),2)),
+            ) %>%
+  mutate(ext.rt = round(npredtrees/nobstrees,2))
+
+names(opt.mean) <- c('N detected trees',
+                     'N reference trees',
+                     'Extraction rate',
+                     'Match rate',
+                     'Overall accuracy',
+                     'Omission rate',
+                     'Commission rate',
+                     'RMS ∆XY',
+                     'RMS ∆Z',
+                     'RMS ∆XYZ',
+                     'Precision',
+                     'Recall',
+                     'F'
+                     )
 
 opt.sd <- opt.res %>%
   summarise(across(nobstrees:loss, \(x) sd(x, na.rm=T)))
 
 # Generate summary stats for best-performing run of each model
-View(itc.means %>%
-  filter(f==max(f)))
+itc.best <- itc.means %>%
+  filter(f==max(f)) %>%
+  group_by(model) %>%
+  filter(row_number()==1) %>%
+  mutate(best=paste0(model, paramset))
+
+best.res <- itc.res %>%
+  mutate(modp=paste0(model,paramset)) %>%
+  filter(modp %in% itc.best$best)
+
+best.mean <- best.res %>%
+  group_by(model) %>%
+  summarise(across(nobstrees:npredtrees, \(x) sum(x, na.rm=T)),
+            across(c(ext.rt, match.rt, accuracy:f), \(x) round(sqrt(mean(x^2, na.rm=T)),2)),
+            .groups='drop'
+  ) %>%
+  mutate(ext.rt = round(npredtrees/nobstrees,2),
+         model=c('Li (2012)',
+                 'LMF-auto',
+                 'LMF fixed window',
+                 'LMF variable window',
+                 'LayerStacking',
+                 'MultiCHM',
+                 'PTrees',
+                 'Watershed'))
+names(best.mean) <- c('Model', names(opt.mean))
+
+# Flextables
+opt.mean.ft <- flextable(opt.mean)
+best.mean.ft <- flextable(best.mean)
+
+opt.mean.ft
+best.mean.ft
