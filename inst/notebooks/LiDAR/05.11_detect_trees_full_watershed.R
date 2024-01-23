fe7e473d80d9c5ab421059b36f0ebfdca38f643e
hmworsham <hmworsham@gmail.com>
2024-01-23 14:29:56 -0800

    clean up after big run

--- inst/notebooks/LiDAR/05.11_detect_trees_full_watershed.R
+++ inst/notebooks/LiDAR/05.11_detect_trees_full_watershed.R
@@ -33,13 +33,19 @@ infiles <- list.files(config$extdata$las_dec, full.names=T)
 lascat <- readLAScatalog(infiles)
 
 ## ---------------------------------------------------------------------------------------------------
+# Set las catalog processing options
+opt_stop_early(lascat) <- F # Proceed through errors, leaving gaps for failed chunks
+lascat@output_options$drivers$sf$param$append <- F # Don't append when writing sf to .shp
+lascat@output_options$drivers$sf$param$delete_dsn <- T # Overwrite existing when writing sf to .shp
+
+## ---------------------------------------------------------------------------------------------------
 # Specify optimal LayerStacking parameters
 start = 0.5
 res = 0.5
 ws1 = 2
 ws2 = 2
 buf = 0.2
-hmin = 1.3
+hmin = 1.8
 
 # Specify optimal lmf-fw parameters
 # ws=2.4
@@ -48,13 +54,12 @@ hmin = 1.3
 ## ---------------------------------------------------------------------------------------------------
 # Specify output location
 opt_output_files(lascat) <- file.path(outdir, 'trees_{XLEFT}_{YBOTTOM}')
-opt_chunk_size(lascat) <- 500
-opt_chunk_buffer(lascat) <- 25
+opt_chunk_size(lascat) <- 100
+opt_chunk_buffer(lascat) <- 10
+# plot(lascat, chunk=T)
+# plot(lascat)
 
 # Detect trees using optimal algorithm and parameters
-plan(multisession, workers=24L)
+algo <- LayerStacking(start=0.5, res=0.5, ws1=2, ws2=2, buf_size=0.2, hardwood=F, hmin=1.8)
 
-#ls.init(lascat[60,], start=0.5, res=0.5, ws1=2, ws2=2, buf_size=0.2, hmin=1.3, hardwood=F)
-algo <- LayerStacking(start=0.5, res=0.5, ws1=2, ws2=2, buf_size=0.2, hardwood=F, hmin=1.3)
-#algo2 <- lmf(ws=ws, shape=shape, hmin=1.3)
-ls.trees <- find_trees(lascat[1:110,], algo, uniqueness='incremental')
+set5 <- split(1:nrow(lascat), ceiling(1:nrow(lascat)/5))
