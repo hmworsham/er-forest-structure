d3e29b23e7dd9b5f9ec4b74ba5e517c621098b3d
hmworsham <hmworsham@gmail.com>
2024-02-22 10:41:24 -0800

    adjust datadir to use las_remainder_regrid as input

--- inst/notebooks/LiDAR/05.11_detect_trees_full_watershed.R
+++ inst/notebooks/LiDAR/05.11_detect_trees_full_watershed.R
@@ -17,20 +17,21 @@ drive_auth(path=config$drivesa)
 
 ## ---------------------------------------------------------------------------------------------------
 workerNodes <- str_split(system('squeue -u $USER -o "%N"', intern=T)[[2]], ',', simplify=T)
-workerNodes <- rep(workerNodes, 32)
+workerNodes <- rep(workerNodes, 24)
 set_lidr_threads(length(workerNodes)-2)
 
 ## ---------------------------------------------------------------------------------------------------
 # Specify directories
 scrdir <- file.path('/global', 'scratch', 'users', 'worsham')
-datadir <- file.path(scrdir, 'las_decimated')
-outdir <- file.path(scrdir, 'trees')
+datadir <- file.path(scrdir, 'las_remainder_regrid2')
+outdir <- file.path(scrdir, 'trees_ls_50m_remainder')
 dir.create(outdir)
 
 ## ---------------------------------------------------------------------------------------------------
 # Ingest full LAS catalog of decimated points
-infiles <- list.files(config$extdata$las_dec, full.names=T)
-lascat <- readLAScatalog(infiles)
+infiles <- list.files(datadir, full.names=T)
+lascat <- readLAScatalog(infiles[1:592])
+plot(lascat['Number.of.point.records'], lwd=0.1)
 
 ## ---------------------------------------------------------------------------------------------------
 # Set las catalog processing options
@@ -47,19 +48,17 @@ ws2 = 2
 buf = 0.2
 hmin = 1.8
 
-# Specify optimal lmf-fw parameters
-# ws=2.4
-# shape='circular'
-
 ## ---------------------------------------------------------------------------------------------------
 # Specify output location
 opt_output_files(lascat) <- file.path(outdir, 'trees_{XLEFT}_{YBOTTOM}')
-opt_chunk_size(lascat) <- 100
+opt_chunk_size(lascat) <- 50
 opt_chunk_buffer(lascat) <- 10
-# plot(lascat, chunk=T)
 # plot(lascat)
 
 # Detect trees using optimal algorithm and parameters
 algo <- LayerStacking(start=0.5, res=0.5, ws1=2, ws2=2, buf_size=0.2, hardwood=F, hmin=1.8)
 
-set5 <- split(1:nrow(lascat), ceiling(1:nrow(lascat)/5))
+# set5 <- split(1:nrow(lascat), ceiling(1:nrow(lascat)/5))
+
+plan(multisession, workers=30L)
+ls.trees <- find_trees(lascat, algo, uniqueness='bitmerge')
