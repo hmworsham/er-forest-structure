400a5f9d23ad304e6a4292708832bd7239f760f3
hmworsham <hmworsham@gmail.com>
2024-03-05 16:00:46 -0800

    run chm on full watershed

--- inst/notebooks/LiDAR/07.03_predict_species.R
+++ inst/notebooks/LiDAR/07.03_predict_species.R
@@ -22,7 +22,7 @@ nCores <- as.integer(availableCores()-2)
 ## Data ingest
 
 # Ingest detected trees
-alltrees <- read_csv(file.path(config$extdata$scratch, 'trees_masked_100m.csv'))
+alltrees <- read_csv(file.path(config$extdata$scratch, 'trees_masked_5m.csv'))
 
 # Ingest Nicola's classification map
 tmpfile <- drive_download(
@@ -49,27 +49,34 @@ sp.codes <- sp.codes %>%
 infiles <- list.files(config$extdata$las_dec, full.names=T)
 lascat <- readLAScatalog(infiles)
 
+plan(multisession, workers=nCores)
 opt_output_files(lascat) <- file.path(config$extdata$scratch, 'chm', 'chm_{XLEFT}_{YBOTTOM}')
 opt_chunk_size(lascat) <- 500
 opt_chunk_buffer(lascat) <- 10
-set_lidr_threads(nCores)
+#set_lidr_threads(nCores)
 
-lc <- lascat[825,]
-chm.pitfree.025 <- rasterize_canopy(lc, 0.5, p2r(.2), pkg='terra')
+chm.pitfree.025 <- rasterize_canopy(lascat, 0.5, p2r(.2), pkg='terra', overwrite=T)
+
+plot(chm.pitfree.025)
+
 plot(chm.pitfree.025)
 kernel <- matrix(1,7,7)
 chm.smooth <- focal(chm.pitfree.025, w = kernel, fun = mean, na.rm = TRUE)
 plot(chm.smooth)
 
-# Get tree crown objects at plots
+# Get tree crown objects
 stem.sf <- st_as_sf(alltrees, coords=c('X', 'Y'), crs=32613)
 stem.sf.crp <- st_crop(stem.sf, ext(chm.smooth))
+# stem.sf.crp <- stem.sf[st_as_sfc(st_bbox(chm.smooth)),]
 st_crs(stem.sf.crp) <- st_crs(chm.smooth)
 stem.sf.crp$treeID <- 1:nrow(stem.sf.crp)
-crowns_poly <- mcws(stem.sf.crp, chm.smooth, minHeight=3, format='polygons')
-plot(crowns_ras, col='blue')
+plot(stem.sf.crp, add=T)
 
-stem.seg <- st_join(st_as_sf(crowns_poly), stem.sf, by=c('treeID'))
+crowns_poly <- mcws(stem.sf.crp, chm.smooth, minHeight=quantile(stem.sf.crp$H, .8), format='polygons')
+
+plot(crowns_poly, col=NA, add=T)
+
+stem.seg <- st_join(st_as_sf(crowns_poly), stem.sf.crp, by=c('treeID'))
 
 # Filter only top trees
 stem.filt.e <- stem.seg %>%
