15935958743572577b84d58c64323144602eb813
hmworsham <hmworsham@gmail.com>
2024-03-07 09:22:27 -0800

    move segmentation to scratch and proceed with height-dependent buffer

--- inst/notebooks/LiDAR/07.03_predict_species.R
+++ inst/notebooks/LiDAR/07.03_predict_species.R
@@ -22,7 +22,12 @@ nCores <- as.integer(availableCores()-2)
 ## Data ingest
 
 # Ingest detected trees
+# treefiles <- list.files('/global/scratch/users/worsham/trees_ls_100m_csv', pattern='.csv', full.names=T)
+# trees <- mclapply(treefiles, read.csv, mc.cores=getOption('mc.cores', nCores))
 alltrees <- read_csv(file.path(config$extdata$scratch, 'trees_masked_5m.csv'))
+
+# Cast trees as sf
+stem.sf <- st_as_sf(alltrees, coords=c('X', 'Y'), crs=32613)
 
 # Ingest Nicola's classification map
 tmpfile <- drive_download(
@@ -49,30 +54,44 @@ sp.codes <- sp.codes %>%
 infiles <- list.files(config$extdata$las_dec, full.names=T)
 lascat <- readLAScatalog(infiles)
 
-plan(multisession, workers=nCores)
-opt_output_files(lascat) <- file.path(config$extdata$scratch, 'chm', 'chm_{XLEFT}_{YBOTTOM}')
-opt_chunk_size(lascat) <- 500
-opt_chunk_buffer(lascat) <- 10
-#set_lidr_threads(nCores)
+## ---------------------------------------------------------------------------------------------------
+## Assign species to tree objects using height-dependent buffer for trees > 90th percentile height
 
-chm.pitfree.025 <- rasterize_canopy(lascat, 0.5, p2r(.2), pkg='terra', overwrite=T)
+stem.sf <- stem.sf %>%
+  mutate(Crown_Radius=0.082*H + 0.5) %>%
+  filter(H >= quantile(H, 0.9))
 
-plot(chm.pitfree.025)
+stem.buff <- st_buffer(stem.sf, dist=0.5*stem.sf$Crown_Radius,
+                       endCapStyle = 'SQUARE', joinStyle='MITRE')
 
-plot(chm.pitfree.025)
-kernel <- matrix(1,7,7)
-chm.smooth <- focal(chm.pitfree.025, w = kernel, fun = mean, na.rm = TRUE)
-plot(chm.smooth)
+stems.spp <- exactextractr::exact_extract(sp.class, stem.buff, 'mode', progress=T)
+stems.spp.sf <- cbind(stem.sf, 'Pixel_Code'=stems.spp)
+stems.spp.sf <- left_join(stems.spp.sf, sp.codes, by='Pixel_Code')
 
-# Get tree crown objects
-stem.sf <- st_as_sf(alltrees, coords=c('X', 'Y'), crs=32613)
-stem.sf.crp <- st_crop(stem.sf, ext(chm.smooth))
+data.table::fwrite(stems.spp.sf,
+                   file.path(config$extdata$scratch, 'stems_species.csv'))
+
+## ---------------------------------------------------------------------------------------------------
+## SCRATCH
+
+## Crown segmentation approach
+
+# Cast tree crown objects as sf
+# stem.sf.crp <- st_crop(stem.sf, ext(chm.smooth))
 # stem.sf.crp <- stem.sf[st_as_sfc(st_bbox(chm.smooth)),]
-st_crs(stem.sf.crp) <- st_crs(chm.smooth)
-stem.sf.crp$treeID <- 1:nrow(stem.sf.crp)
-plot(stem.sf.crp, add=T)
+# st_crs(stem.sf.crp) <- st_crs(chm.smooth)
+# stem.sf.crp$treeID <- 1:nrow(stem.sf.crp)
+# plot(stem.sf.crp, add=T)
 
-crowns_poly <- mcws(stem.sf.crp, chm.smooth, minHeight=quantile(stem.sf.crp$H, .8), format='polygons')
+full.mask <- rast(file.path(config$extdata$scratch, 'tifs', 'fullmask_5m.tif'))
+ext(full.mask) <- ext(chm.smooth)
+full.mask <- alignfun(full.mask, chm.smooth)
+
+chm.masked <- mask(chm.smooth, full.mask)
+
+crowns_poly <- mcws(stem.sf, chm.masked, minHeight=quantile(stem.sf$H, .8), format='polygons')
+
+st_write(crowns_poly)
 
 plot(crowns_poly, col=NA, add=T)
 
@@ -95,22 +114,3 @@ stem.filt.e <- stem.seg %>%
   filter(Zpred >= quantile(Zpred, 0.9)) %>%
   ungroup() %>%
   mutate(Crown_Area=st_area(.))
-
-# Plot CHM
-crowns_poly <- lapply(chm.smooth, mcws, treetops = stem.sf, minHeight = 3, format='polygons')
-plot(chm.smooth[[10]], xlab = "", ylab = "", xaxt='n', yaxt = 'n')
-plot(crowns_poly[[10]]$geometry, border = "blue", lwd = 0.5, add = TRUE)
-
-stem.seg <- do.call('rbind', crowns_poly)
-stem.seg <- st_join(st_as_sf(stem.seg), stem.sf, by=c('treeID'))
-
-# Filter only top trees
-stem.filt.e <- stem.seg %>%
-  group_by(Site_Name) %>%
-  filter(Zpred >= quantile(Zpred, 0.9)) %>%
-  ungroup() %>%
-  mutate(Crown_Area=st_area(.))
-
-# Shrink crowns
-stem.filt.e <- st_buffer(stem.filt.e, -0.2*sqrt(stem.filt.e$Crown_Area))
-
