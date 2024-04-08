f6f509d9a3e5e8778fb32d5ea98cd87f436b5a7d
hmworsham <hmworsham@gmail.com>
2024-04-08 12:04:08 -0600

    major refactor with grid and patchwork for publication-ready figures

--- inst/notebooks/regressions/05.01_gbm_reports.R
+++ inst/notebooks/regressions/05.01_gbm_reports.R
@@ -34,6 +34,8 @@ names(gbms) <- gbm.names.new$n[x1]
 varnames <- read.csv(file.path(config$data$int, 'explainer_names_table.csv'),
                      row.names=1)
 
+varnames[varnames$label=='\xc6SWE', 'label'] <- '\u0394SWE'
+
 #############
 # Summaries
 #############
@@ -165,6 +167,119 @@ gbm.sums %>%
 # Plots
 ######################
 
+# Group by plot
+gbm.summaries.cat <- gbm.sums %>%
+  mutate(category = factor(category, levels=c('Climate', 'Topography',
+                                              'Soil', 'Geology')),
+         label = factor(label,
+                        levels=c('AET', 'CWD', 'SWE', '\u0394SWE',
+                                        'Curvature', 'Elevation', 'Heat load',
+                                        'TPI', 'TWI',
+                                        'AWC', 'CEC', 'ksat',
+                                        'Organic matter', 'pH', 'Silt content',
+                                        'Geology'),
+                        labels=c('AET', 'CWD', 'SWE', '\u0394SWE',
+                                 'Curvature', 'Elevation', 'Heat load',
+                                 'TPI', 'TWI',
+                                 'AWC', 'CEC', expression(k['sat']),
+                                 'Organic matter', 'pH', 'Silt content',
+                                 'Geology')),
+         var = factor(var)) %>%
+  arrange(category, label)
+
+soil.labs <- c('AWC', 'CEC', expression(k['sat']),
+'Organic matter', 'pH', 'Silt content')
+
+# Plot importance for each structure metric
+
+p1 <- ggplot(gbm.summaries.cat, aes(x=Model, y=rel.inf, fill=interaction(category, label, sep=': '))) +
+  geom_col(position='stack') +
+  scale_fill_manual(limits=interaction(gbm.summaries.cat$category, gbm.summaries.cat$label, sep=': '),
+                    values=gbm.summaries.cat$pdcolors, name=NULL) +
+  #scale_fill_manual(values=unname(nifty::icolors('pantone')[c(1,4,5,7)])) +
+  #scale_fill_manual(values = c(mypal(4), mypal2(4), mypal3(4), mypal4(1))) +
+  labs(x='Response', y='Relative influence (%)') +
+  ggthemes::theme_calc(base_size=8,
+                       base_family='Arial') +
+  guides(fill = guide_legend(byrow = TRUE)) +
+  theme(axis.text.x=element_text(angle=60, hjust=1),
+        legend.position='none',
+        # legend.direction = 'horizontal',
+        legend.spacing.y = unit(0.1, 'mm'),
+        # legend.text = element_text(size=rel(0.7)),
+        legend.key.size = unit(0.8, 'lines'),
+        legend.margin = margin(r=1, l=0, t=10, b=10),
+        plot.background = element_rect(color = NA)
+        )
+
+p1
+
+gbm.sum.tbl <- split(gbm.summaries.cat, gbm.summaries.cat$category)
+# gbm.sum.tbl <- lapply(gbm.sum.tbl, \(x) {
+#   #x$label=as.character(x$label)
+#   x %>%
+#     arrange(x, label, Model)
+#   x})
+
+gbm.col.tbl <- lapply(gbm.sum.tbl, \(x) {
+  x %>%
+    select(label, pdcolors) %>%
+    deframe()
+})
+
+p1.leg <- ggplot(mapping=aes(x=Model, y=rel.inf)) +
+  geom_col(data=gbm.sum.tbl$Climate, position='stack', aes(fill=label)) +
+  scale_fill_manual(values=gbm.col.tbl$Climate,
+                    name='Climate',
+                    guide=guide_legend(order=1)) +
+  ggnewscale::new_scale_fill() +
+  geom_col(data=gbm.sum.tbl$Topography, position='stack', aes(fill=label)) +
+  scale_fill_manual(values=gbm.col.tbl$Topography,
+                    name='Topography',
+                    guide=guide_legend(order=2)) +
+  ggnewscale::new_scale_fill() +
+  geom_col(data=gbm.sum.tbl$Soil, position='stack', aes(fill=label)) +
+  scale_fill_manual(values=gbm.col.tbl$Soil,
+                    name='Soil',
+                    guide=guide_legend(order=3),
+                    labels=soil.labs) +
+  ggnewscale::new_scale_fill() +
+  geom_col(data=gbm.sum.tbl$Geology, position='stack', aes(fill=label)) +
+  scale_fill_manual(values=gbm.col.tbl$Geology,
+                    name='Geology',
+                    guide=guide_legend(order=4)) +
+  theme(legend.position = "left",
+        legend.box = "vertical",
+        legend.direction='vertical',
+        legend.box.just = "left",
+        legend.title = element_text(size=rel(1))) +
+  labs(x='Model', y='Relative influence (%)') +
+  ggthemes::theme_calc(base_size=7,
+                       base_family='Arial') +
+  guides(fill = guide_legend(byrow = TRUE)) +
+  theme(axis.text.x=element_text(angle=60, hjust=1),
+        legend.position='left',
+        # legend.direction = 'horizontal',
+        legend.spacing.y = unit(0.1, 'mm'),
+        legend.text = element_text(hjust=0),
+        # legend.text = element_text(size=rel(0.7)),
+        legend.key.size = unit(0.8, 'lines'),
+        legend.margin = margin(r=1, l=1, t=2, b=5),
+        legend.title = element_text(size=rel(1),
+                                    face='bold'),
+        #panel.background = element_rect(color = NA)
+        plot.background = element_rect(color = NA)
+  )
+
+g_legend <- function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  legend
+}
+
+p1.leg <- g_legend(p1.leg)
+
 # Facet wrap top 10 variables
 
 reorder_within <- function(x, by, within, fun = mean, sep = "___", ...) {
@@ -177,54 +292,103 @@ scale_x_reordered <- function(..., sep = "___") {
   ggplot2::scale_x_discrete(labels = function(x) gsub(reg, "", x), ...)
 }
 
-p1 <- ggplot(gbm.summaries.5, aes(x=reorder_within(label, rel.inf, Model), y=rel.inf,
+gbm.summaries.5.all <- gbm.summaries.5[!gbm.summaries.5$Model %in%
+                                         c('Fir density', 'Spruce density',
+                                           'Pine density'),]
+
+p2 <- ggplot(gbm.summaries.5.all, aes(x=reorder_within(label, rel.inf, Model), y=rel.inf,
                                   fill=label)) +
   geom_col() +
   scale_x_reordered() +
   coord_flip() +
   scale_fill_manual(limits=gbm.summaries.5$label, values=gbm.summaries.5$pdcolors,
                     guide='none') +
-  labs(x='Predictor variable', y='Relative influence') +
+  labs(x='Abiotic explanatory variable', y='Relative influence (%)') +
   facet_wrap(~Model, scales='free_y') +
-  ggthemes::theme_calc(base_size=18)
-# +
-# theme(axis.text = element_text(size = 12),
-#       plot.title = element_text(size=12),
-#       strip.text = element_text(size=14),
-#       strip.text.x = element_text(size=14),
-#       strip.text.y = element_text(size=14))
-
-p1
-
-# Group by plot
-gbm.summaries.cat <- gbm.sums %>%
-  mutate(category = factor(category, levels=c('Climate', 'Topography',
-                                              'Soil', 'Geology')),
-         label = factor(label, levels=c('AET', 'CWD', 'SWE', '∆SWE',
-                                         'Curvature', 'Elevation', 'Heat load',
-                                            'TPI', 'TWI',
-                                         'AWC', 'CEC', 'Organic matter',
-                                            'Silt content','ksat', 'pH',
-                                         'Geology')),
-         var = factor(var)) %>%
-  arrange(category, label)
-
-# Plot importance for each structure metric
-
-p2 <- ggplot(gbm.summaries.cat, aes(x=Model, y=rel.inf, fill=interaction(category, label, sep=': '))) +
-  geom_col(position='stack') +
-  scale_fill_manual(limits=interaction(gbm.summaries.cat$category, gbm.summaries.cat$label, sep=': '),
-                    values=gbm.summaries.cat$pdcolors, name=NULL) +
-  #scale_fill_manual(values=unname(nifty::icolors('pantone')[c(1,4,5,7)])) +
-  #scale_fill_manual(values = c(mypal(4), mypal2(4), mypal3(4), mypal4(1))) +
-  labs(x='Relative influence', y='Predictor variable') +
-  ggthemes::theme_calc(base_size=18) +
-  theme(axis.text.x=element_text(angle=60, hjust=1),
-        legend.position='left')
+  ggthemes::theme_calc(base_size=8,
+                       base_family='Arial')
 
 p2
 
-gridExtra::grid.arrange(p2, p1, ncol=2, widths=c(6,5))
+gbm.summaries.5.spp <- gbm.summaries.5[gbm.summaries.5$Model %in%
+                                         c('Fir density', 'Spruce density',
+                                           'Pine density'),]
+
+p3 <- ggplot(gbm.summaries.5.spp, aes(x=reorder_within(label, rel.inf, Model),
+                                      y=rel.inf, fill=label)) +
+  geom_col() +
+  scale_x_reordered() +
+  coord_flip() +
+  scale_fill_manual(limits=gbm.summaries.5$label, values=gbm.summaries.5$pdcolors,
+                    guide='none') +
+  labs(x='Abiotic explanatory variable', y='Relative influence (%)') +
+  facet_wrap(~Model, scales='free_y') +
+  ggthemes::theme_calc(base_size=8,
+                       base_family='Arial')
+
+#gridExtra::grid.arrange(p2, p1, ncol=2, widths=c(6,5))
+
+# loadfonts(device='pdf')
+# setEPS()
+# postscript('~/Desktop/Fig6.eps', width=190/25.4, height=160/25.4,
+#             onefile=T, family='Arial Unicode MS', paper='special', bg='white', horizontal=T)
+#
+# pdf('~/Desktop/Fig6.pdf', width=190/25.4, height=100/25.4, onefile=T,
+#     family='Arial', bg='white', paper='special', encoding='ISOLatin2.enc')
+#
+
+cairo_pdf('~/Desktop/Fig6.pdf', width=190/25.4, height=120/25.4, onefile=T,
+          family='Arial', bg='white')
+
+grid.newpage()
+
+vp1 <- viewport(x = 0, y = 0,
+                height = 1, width = 0.12,
+                just = c("left", "bottom"),
+                name = "leg1")
+pushViewport(vp1)
+#grid.rect()
+grid.draw(p1.leg)
+grid.text('(A)', x=0.04, y=0.02, just=c('left', 'bottom'))
+
+upViewport(1)
+vp2 <- viewport(x=0.12, y=0,
+                height=1, width=0.38,
+                just = c('left', 'bottom'),
+                name='p1')
+pushViewport(vp2)
+print(p1, newpage=F)
+
+upViewport(1)
+vp3 <- viewport(x=1, y=0.4,
+                height=0.6, width=0.5,
+                just=c('right', 'bottom'),
+                name='p2')
+pushViewport(vp3)
+#grid.rect()
+print(p2, newpage=F)
+grid.text('(B)', x=0.02, y=0.03, just=c('left', 'bottom'))
+
+upViewport(1)
+vp4 <- viewport(x=1, y=0,
+                height=0.4, width=0.5,
+                just=c('right', 'bottom'),
+                name='p3')
+pushViewport(vp4)
+#grid.rect()
+print(p3, newpage=F)
+grid.text('(C)', x=0.02, y=0.04, just=c('left', 'bottom'))
+
+
+upViewport(1)
+vp5 <- viewport(x=0, y=0,
+                height=1, width=0.5,
+                just=c('left', 'bottom'),
+                name='vp4')
+pushViewport(vp5)
+grid.rect(gp=gpar(fill=NA))
+
+dev.off()
 
 #############
 # SCRATCH
