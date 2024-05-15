370143e0782e3ebb53775d901ec9d6f42a0d7b6e
hmworsham <hmworsham@gmail.com>
2024-05-15 10:16:54 -0600

    functionalize DBH estimation error plot for export

--- inst/notebooks/LiDAR/07.00_predict_dbh.R
+++ inst/notebooks/LiDAR/07.00_predict_dbh.R
@@ -2,11 +2,7 @@
 
 ## ---------------------------------------------------------------------------------------------------
 # Load config
-config <- config::get(file=file.path('~',
-                                     'Repos',
-                                     'er-forest-structure',
-                                     'config',
-                                     'config.yml'))
+config <- config::get(file=file.path('config','config.yml'))
 
 # Load local helper functions and packages
 devtools::load_all()
@@ -18,7 +14,7 @@ drive_auth(path=config$drivesa)
 # Set number of cores
 nCores <- as.integer(availableCores()-2)
 
-## ---------------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 ## Data ingest
 
 # Ingest trees
@@ -55,10 +51,9 @@ allom <- inv %>%
   mutate(Tree_ID=as.character(Tree_ID)) %>%
   na.omit()
 
-# Scatterplot with a smooth fit
+# Scatterplot
 ggplot(allom, aes(x=Height_Avg_M, y=DBH_Avg_CM)) +
   geom_point(shape=21) +
-  geom_smooth(formula=log(y)~log(x), se=F) +
   labs(x='Height (m)', y='DBH (cm)')
 
 # Break data into bins
@@ -95,104 +90,109 @@ colnames(bin.nl.model)[1:8] <- c('alpha', 'beta', 'gamma', 'sigma',
                               'sigma_v', 'RMSE', 'bias', 'CV')
 
 ## Create empty plot to visualize model errors for each randomization step
-plot(1,1,log='x', pch=16, col="white",bty="l",xlim=c(1,200),ylim=c(-100,100),
-     xlab="log of observed diameter (cm)",ylab="Error (%)",las=1,cex.axis=0.8,cex.lab=0.9)
-grid(equilogs=FALSE,lty=2,col="grey80",lwd=0.5)
-abline(h=0,col="grey20",lty=2)
-legend("bottomleft",lwd=2,col=c("forestgreen","grey20"),
-       c("Model fit to raw data",
-         "Model fit to binned data",
-         'NLS model fit to binned data'), bty="n", cex=0.8)
+err.rand.plt <- function() {
+  plot(1,1,log='x', pch=16, col="white",bty="l",xlim=c(1,200),ylim=c(-100,100),
+       xlab="Observed diameter (cm)",ylab="Error (%)",las=1,cex.axis=0.8,cex.lab=0.9)
+  grid(equilogs=FALSE,lty=2,col="grey80",lwd=0.5)
+  abline(h=0,col="grey20",lty=2)
+  legend("bottomleft",lwd=2,col=c("forestgreen","grey20", 'dodgerblue'),
+         c(as.expression(bquote("log(DBH) ~"~alpha + beta~log(H[raw])~x~e^(σ^2/2))),
+           as.expression(bquote("log(DBH) ~"~alpha + beta~log(H[binned])~x~e^(σ^2/2))),
+           as.expression(bquote("log(DBH) ~"~alpha + log(H[binned]-beta)^gamma~x~e^(σ^2/2)))
+         ),
+                                       bty="n", cex=0.8)
 
-for (i in 1:reps){
+  for (i in 1:reps){
 
-  #### Set up validation and model datasets
-  val.data <- allom.bins %>%
-    group_by(bin) %>%
-    slice_sample(prop=0.25) %>%
-    ungroup() %>%
-    rename(H=Height_Avg_M,
-           DBH=DBH_Avg_CM)
+    #### Set up validation and model datasets
+    val.data <- allom.bins %>%
+      group_by(bin) %>%
+      slice_sample(prop=0.25) %>%
+      ungroup() %>%
+      rename(H=Height_Avg_M,
+             DBH=DBH_Avg_CM)
 
-  mod.trees <- data.frame(Tree_ID=setdiff(allom.bins$Tree_ID, val.data$Tree_ID))
+    mod.trees <- data.frame(Tree_ID=setdiff(allom.bins$Tree_ID, val.data$Tree_ID))
 
-  mod.data <- merge(allom.bins, mod.trees) %>%
-    rename(H=Height_Avg_M,
-           DBH=DBH_Avg_CM)
+    mod.data <- merge(allom.bins, mod.trees) %>%
+      rename(H=Height_Avg_M,
+             DBH=DBH_Avg_CM)
 
-  #### Data binning: calculate mean values per size class for each allometric component
-  bin.data <- mod.data %>%
-    group_by(bin) %>%
-    summarise(n.trees=length(DBH),
-              DBH=mean(DBH),
-              H=mean(H)) %>%
-    filter(n.trees>2)
+    #### Data binning: calculate mean values per size class for each allometric component
+    bin.data <- mod.data %>%
+      group_by(bin) %>%
+      summarise(n.trees=length(DBH),
+                DBH=mean(DBH),
+                H=mean(H)) %>%
+      filter(n.trees>2)
 
-  #### Fit models and generate predictions
+    #### Fit models and generate predictions
 
-  ## Model 1: model fit to raw data
-  m1 <- lm(log(DBH) ~ log(H), data=mod.data)
-  val.data$DBH_pred_m1 <- exp(predict(m1, val.data))*exp(summary(m1)$sigma^2/2)
-  val.data$error_m1 <- relative_err(val.data$DBH, val.data$DBH_pred_m1)
-  lines(smooth.spline(val.data$error_m1 ~ val.data$DBH, nknots=5),
-        col='forestgreen',
-        lwd=0.5)
+    ## Model 1: model fit to raw data
+    m1 <- lm(log(DBH) ~ log(H), data=mod.data)
+    val.data$DBH_pred_m1 <- exp(predict(m1, val.data))*exp(summary(m1)$sigma^2/2)
+    val.data$error_m1 <- relative_err(val.data$DBH, val.data$DBH_pred_m1)
+    lines(smooth.spline(val.data$error_m1 ~ val.data$DBH, nknots=5),
+          col='forestgreen',
+          lwd=0.5)
 
-  ## Model 2: model fit to binned data
-  m2 <- lm(log(DBH) ~ log(H), data=bin.data)
-  val.data$DBH_pred_m2 <- exp(predict(m2, val.data))*exp(summary(m2)$sigma^2/2)
-  val.data$error_m2 <- relative_err(val.data$DBH, val.data$DBH_pred_m2)
-  lines(smooth.spline(val.data$error_m2 ~ val.data$DBH, nknots=5),
-        col='grey20',
-        lwd=0.5)
+    ## Model 2: model fit to binned data
+    m2 <- lm(log(DBH) ~ log(H), data=bin.data)
+    val.data$DBH_pred_m2 <- exp(predict(m2, val.data))*exp(summary(m2)$sigma^2/2)
+    val.data$error_m2 <- relative_err(val.data$DBH, val.data$DBH_pred_m2)
+    lines(smooth.spline(val.data$error_m2 ~ val.data$DBH, nknots=5),
+          col='grey20',
+          lwd=0.5)
 
-  ## Model 3: nonlinear model fit to binned data
-  m3 <- nls(log(DBH) ~ a + log((H-b)^g),
-            start=list(a=1.6, b=1.3, g=.8),
-            weights=H,
-            lower=c(0, 1, 0.2),
-            algorithm='port',
-            data=bin.data,
-            na.action=na.exclude,
-            control=nls.control(maxiter=1000))
-  val.data$DBH_pred_m3 <- exp(predict(m3, val.data))*exp(summary(m3)$sigma^2/2)
-  val.data$error_m3 <- relative_err(val.data$DBH, val.data$DBH_pred_m3)
-  lines(smooth.spline(val.data$error_m3 ~ val.data$DBH, nknots=5),
-        col='dodgerblue',
-        lwd=0.5)
+    ## Model 3: nonlinear model fit to binned data
+    m3 <- nls(log(DBH) ~ a + log((H-b)^g),
+              start=list(a=1.6, b=1.3, g=.8),
+              weights=H,
+              lower=c(0, 1, 0.2),
+              algorithm='port',
+              data=bin.data,
+              na.action=na.exclude,
+              control=nls.control(maxiter=1000))
+    val.data$DBH_pred_m3 <- exp(predict(m3, val.data))*exp(summary(m3)$sigma^2/2)
+    val.data$error_m3 <- relative_err(val.data$DBH, val.data$DBH_pred_m3)
+    lines(smooth.spline(val.data$error_m3 ~ val.data$DBH, nknots=5),
+          col='dodgerblue',
+          lwd=0.5)
 
-  #### Compute and store model error statistics
+    #### Compute and store model error statistics
 
-  ## Model fit to raw data
-  raw.model[i,"alpha"]<-coef(m1)[1]
-  raw.model[i,"beta"]<-coef(m1)[2]
-  raw.model[i,"sigma"]<-summary(m1)$sigma
-  raw.model[i,"RMSE"]<-RMSE(val.data$DBH, val.data$DBH_pred_m1)
-  raw.model[i,"bias"]<-bias(val.data$DBH, val.data$DBH_pred_m1)
-  raw.model[i,"CV"]<-CV(val.data$DBH ,val.data$DBH_pred_m1)
+    ## Model fit to raw data
+    raw.model[i,"alpha"]<-coef(m1)[1]
+    raw.model[i,"beta"]<-coef(m1)[2]
+    raw.model[i,"sigma"]<-summary(m1)$sigma
+    raw.model[i,"RMSE"]<-RMSE(val.data$DBH, val.data$DBH_pred_m1)
+    raw.model[i,"bias"]<-bias(val.data$DBH, val.data$DBH_pred_m1)
+    raw.model[i,"CV"]<-CV(val.data$DBH ,val.data$DBH_pred_m1)
 
-  ## Model fit to binned data
-  bin.model[i,"alpha"]<-coef(m2)[1]
-  bin.model[i,"beta"]<-coef(m2)[2]
-  bin.model[i,"sigma"]<-summary(m2)$sigma
-  bin.model[i,"sigma_v"]<-sigma_v(val.data$DBH, val.data$DBH_pred_m2)
-  bin.model[i,"RMSE"]<-RMSE(val.data$DBH,val.data$DBH_pred_m2)
-  bin.model[i,"bias"]<-bias(val.data$DBH,val.data$DBH_pred_m2)
-  bin.model[i,"CV"]<-CV(val.data$DBH, val.data$DBH_pred_m2)
+    ## Model fit to binned data
+    bin.model[i,"alpha"]<-coef(m2)[1]
+    bin.model[i,"beta"]<-coef(m2)[2]
+    bin.model[i,"sigma"]<-summary(m2)$sigma
+    bin.model[i,"sigma_v"]<-sigma_v(val.data$DBH, val.data$DBH_pred_m2)
+    bin.model[i,"RMSE"]<-RMSE(val.data$DBH,val.data$DBH_pred_m2)
+    bin.model[i,"bias"]<-bias(val.data$DBH,val.data$DBH_pred_m2)
+    bin.model[i,"CV"]<-CV(val.data$DBH, val.data$DBH_pred_m2)
 
-  ## NLS model fit to binned data
-  bin.nl.model[i,"alpha"] <- coef(m3)[1]
-  bin.nl.model[i,"beta"] <- coef(m3)[2]
-  bin.nl.model[i,'gamma'] <- coef(m3)[3]
-  bin.nl.model[i,"sigma"] <- summary(m3)$sigma
-  bin.nl.model[i,"sigma_v"] <- sigma_v(val.data$DBH, val.data$DBH_pred_m3)
-  bin.nl.model[i,"RMSE"] <- RMSE(val.data$DBH, val.data$DBH_pred_m3)
-  bin.nl.model[i,"bias"] <- bias(val.data$DBH, val.data$DBH_pred_m3)
-  bin.nl.model[i,"CV"] <- CV(val.data$DBH, val.data$DBH_pred_m3)
+    ## NLS model fit to binned data
+    bin.nl.model[i,"alpha"] <- coef(m3)[1]
+    bin.nl.model[i,"beta"] <- coef(m3)[2]
+    bin.nl.model[i,'gamma'] <- coef(m3)[3]
+    bin.nl.model[i,"sigma"] <- summary(m3)$sigma
+    bin.nl.model[i,"sigma_v"] <- sigma_v(val.data$DBH, val.data$DBH_pred_m3)
+    bin.nl.model[i,"RMSE"] <- RMSE(val.data$DBH, val.data$DBH_pred_m3)
+    bin.nl.model[i,"bias"] <- bias(val.data$DBH, val.data$DBH_pred_m3)
+    bin.nl.model[i,"CV"] <- CV(val.data$DBH, val.data$DBH_pred_m3)
 
+  }
 }
 
-# Estimating uncertainty for models fit to binned data -------------
+
+# Estimating uncertainty for models fit to binned data
 
 # Final model - updates parameters based on means from prior runs
 mean.raw.coef <- summarise(raw.model, across(alpha:CV, \(x) sqrt(mean(x^2))))
@@ -214,6 +214,7 @@ plot(DBH ~ H, val.data,
 axis(1,at=c(0.5,5,50,500,5000),c("0.5","5","50","500","5000"),cex.lab=1.1,cex.axis=0.9)
 
 new.dat <- data.frame(H=exp(H.new))
+new.dat$DBH_pred <- exp(-mean.coef$alpha + mean.coef$beta*log(new.dat$H))*exp(mean.coef$sigma^2/2)
 new.dat$DBH_pred <- exp(predict(m2,new.dat))*exp(summary(m2)$sigma^2/2)
 points(DBH_pred ~ H, new.dat[new.dat$DBH_pred>1,], type="l", col="red", lwd=3)
 
@@ -247,18 +248,13 @@ allom.val <- inv %>%
   sample_n(800) %>%
   na.omit()
 
-# Scatterplot with a smooth fit
+# Scatterplot
 ggplot(allom.val, aes(x=log(H), y=log(DBH))) +
   geom_point(shape=21) +
-  #geom_smooth(formula='y~x', se=F) +
   labs(x='Height (m)', y='DBH (cm)')
 
 # Predict DBH from original
-allom.val$DBH_pred <- exp(predict(m2,allom.val))*exp(summary(m2)$sigma^2/2)
-# allom.val$DBH_pred <- exp(summary(m2)$coefficients[1] + summary(m2)$coefficients[2]*log(allom.val$H)) *
-#   exp(summary(m2)$sigma^2/2)
-# allom.val$DBH_pred <- exp(summary(m3)$coefficients[1] + log((allom.val$H - summary(m3)$coefficients[2])^
-#                                                               summary(m3)$coefficients[3]))
+allom.val$DBH_pred <- exp(-mean.coef$alpha + mean.coef$beta*log(allom.val$H))*exp(mean.coef$sigma^2/2)
 
 # Compute RMSE, bias, CV.
 allom.val.RMSE <- RMSE(allom.val$DBH, allom.val$DBH_pred)
@@ -275,14 +271,34 @@ allom.val$dens <- col2rgb(dens)[1,] + 1L
 allom.val <- allom.val[order(allom.val$dens),]
 
 ## Plot
-ggplot(allom.val, aes(x=DBH, y=DBH_pred, color=dens)) +
+allom.val.plt <- ggplot(allom.val, aes(x=DBH, y=DBH_pred, color=dens)) +
   geom_point() +
-  scale_color_viridis_c() +
+  scale_color_viridis_c(name='Number of observations',
+                        guide=guide_colorbar(
+                          title.position='right',
+                          title.theme=element_text(angle=-90, hjust=0.5))) +
   geom_abline(intercept=0, slope=1) +
+  labs(x='Field-observed DBH (cm)', y='Allometry-estimated DBH (cm)') +
   coord_fixed(ratio=1, xlim=c(0,100), ylim=c(0,100)) +
-  theme(aspect.ratio = 1)
+  ggthemes::theme_calc(base_size=12, base_family='Arial') +
+  theme(aspect.ratio = 1,
+        legend.key.height=unit(0.1, 'npc'))
 
-## ---------------------------------------------------------------------------------------------------
+cairo_pdf('~/Desktop/FigS3a.pdf', width=190/25.4, height=190/25.4, onefile=T,
+          family='Arial', bg='white')
+
+err.rand.plt()
+
+dev.off()
+
+cairo_pdf('~/Desktop/FigS3b.pdf', width=190/25.4, height=190/25.4, onefile=T,
+          family='Arial', bg='white')
+
+allom.val.plt
+
+dev.off()
+
+## ---------------------------------------------------------------------------------------------
 # Predict diameter from height in model data and write to csv
 
 mclapply(treefiles, \(x) {
@@ -294,7 +310,7 @@ mclapply(treefiles, \(x) {
   H.mean <- mean(log(tf$H)) # mean value of the explanatory variable
   H.sd <- sd(log(tf$H)) # standard deviation of the explanatory variable
 
-  tf$DBH_est <-exp(predict(m2, tf))*exp(summary(m2)$sigma^2/2)
+  tf$DBH_est <- exp(-mean.coef$alpha + mean.coef$beta*log(tf$H))*exp(mean.coef$sigma^2/2)
   tf$DBH_lb <- exp(log(tf$DBH_est)-Prediction_interval(t_crit, sigma.v.mean, n, tf$H, H.mean, H.sd))
   tf$DBH_ub <- exp(log(tf$DBH_est)+Prediction_interval(t_crit, sigma.v.mean, n, tf$H, H.mean, H.sd))
   tf$BA_est <- pi*(tf$DBH_est/2)**2
