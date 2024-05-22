76fbaf4ce91911dfc13b9e9c0f475165814bc5fd
hmworsham <hmworsham@gmail.com>
2024-05-22 12:59:57 -0600

    add replicate_all shell script

---
+++ inst/notebooks/tables_figures/replicate_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Replicate all manuscript and SI figures and tables
+
+RScript fig1_domain.R
+RScript fig2_crownmap.R
+RScript fig3_itd_performance.R
+RScript fig4_height_distribution.R
+RScript fig5_structure_metrics.R
+RScript fig6_structure_histograms.R
+RScript fig7_gbm.R
+RScript fig8_gam_partial_effects.R
+RScript fig9_geology.R
+RScript fig10_gam_interactions.R
+RScript si_figs.R
+RScript si_tables.R
+RScript tables.R
+
