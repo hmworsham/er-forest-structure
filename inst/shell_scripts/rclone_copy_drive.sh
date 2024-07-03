7ba30acb8dd1869dc93bc39c334cfde944934b68
hmworsham <hmworsham@gmail.com>
2024-07-03 13:07:15 -0600

    remove for archive

--- inst/shell_scripts/rclone_copy_drive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Rclone data from watched remote drive to Savio Scratch
-
-src=worsham:/LiDAR/las_downsampled
-scratch=/global/scratch/users/worsham/las_downsampled
-priority=16
-
-nohup nice -n $priority rclone copy --bwlimit 8.15M $src $scratch &
