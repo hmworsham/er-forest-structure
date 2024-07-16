953a201a417edabdbd83ac3d6fe1f49753225866
hmworsham <hmworsham@gmail.com>
2024-07-16 08:56:35 -0600

    clean for archive

--- inst/notebooks/LiDAR/00.01_chunkwf.py
+++ inst/notebooks/LiDAR/00.01_chunkwf.py
@@ -7,11 +7,7 @@
 
 import os
 import sys
-
 from ipyparallel import Client
-
-sys.path.append(str('/global/home/users/worsham/Repos/eastriver/Watershed_Spatial_Dataset/LiDAR/'))
-
 from waveform import chunk_waveforms as cw
 
 # Connect to ipyparallel
@@ -22,19 +18,7 @@ lview = c.load_balanced_view()
 # cause execution on main process to wait while tasks sent to workers finish
 lview.block = True
 
-# Define directories
-indir = '/global/scratch/users/worsham/waveform_binary'
-outdir = '/global/scratch/users/worsham/waveform_binary_chunks'
-# indir = '/Users/hmworsham/waveformbinary'
-# outdir = '/Users/hmworsham/waveformbinarychunks'
-
-# fps = [d for d in os.listdir(indir) if os.path.isdir(os.path.join(indir, d))]
-
-# importlib.reload(cw)
-# tst = cw.envi_chunk(fps[0], indir, outdir)
-# tst2 = cw.cp_files(fps[0], indir, outdir)
-# tst3 = cw.chunk_wfbinary_loc(fps[0], indir, outdir)
-
+# Define wrapper function for parallel chunking
 def wrapper(i):
     import sys
     sys.path.append(
@@ -45,19 +29,13 @@ def wrapper(i):
     return(cw.chunk_wfbinary_loc(i, idir, odir))
 
 
-#Define flightpaths to ingest
+# Name flightpaths to ingest
 fps = [d for d in os.listdir(indir) if os.path.isdir(
     os.path.join(indir, d))]  # Lists all flightpaths
 
-########
-# In series
-########
-# Split and save
-# for f in fps[:3]:
-   # cw.chunk_wfbinary_loc(f, indir, outdir)
-
-######
+##############
 # Parallel
-######
+##############
+
 # Split and save to scratch
 lview.map(wrapper, fps)
